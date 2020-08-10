218007cc22e73472f09e40105a0dbe28b5357b72
bakaflan <184444280@qq.com>
2020-08-11 00:16:45 +0800

    [ruiling] feat: finish 5-async

--- 5-async/3-fetch/index.js
+++ 5-async/3-fetch/index.js
@@ -1,6 +1,18 @@
 function fetchData(url) {
   // <-- start
   // TODO 23: 通过Fetch API实现异步请求
+  const options = {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
   // end -->
 }
 
--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,11 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+
+  const res = await fetch(url);
+  const resData = await res.json();
+  document.writeln(resData.name);
+
   // end -->
 }
 
