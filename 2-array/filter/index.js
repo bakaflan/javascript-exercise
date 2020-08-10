ee28ff8060d8f28aa68fe865d271254721dbf53c
bakaflan <184444280@qq.com>
2020-08-10 21:37:34 +0800

    [ruiling] feat: finish 2-array

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,14 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(i => i % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = collection.filter((item, index) => {
+    return collection.indexOf(item) === index;
+  });
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
