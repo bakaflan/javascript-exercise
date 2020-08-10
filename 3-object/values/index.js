a14ab5af112b06acbded263d09d3b2dffca66df7
bakaflan <184444280@qq.com>
2020-08-10 21:52:04 +0800

    [ruiling] feat: finish 3-object

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  return Object.values(source)
+    .map(value => parseInt(value, 10))
+    .reduce((pre, cur) => {
+      return pre + cur;
+    });
 }
